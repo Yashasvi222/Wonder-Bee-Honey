94f8239b5f9ede99fb64d81065ad91886dd7ac32
Yashasvi222 <yashasviagrawal7@gmail.com>
2024-04-13 10:48:59 +0530

    Navbar added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -16,26 +16,14 @@ const Navbar = () => {
             <ul className='nav-menu'>
                 <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("store")}}>Store{menu==="store"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("story")}}>Our Story{menu==="story"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("contact")}}>Contact Us{menu==="contact"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("journey")}}>Journey{menu==="journey"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("contact")}}>Contact{menu==="contact"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cart_icon} alt="cart-icon" />
                 <div className="nav-cart-count">0</div>
             </div>
-        </div>
-        <div className="hero">
-            
-        </div>
-        <div className="quote">
-
-        </div>
-        <div className="usp">
-
-        </div>
-        <div className="footer">
-
         </div>
     </>
     
