003fce39955eca0f6b6e4ead2547d21bde0c68b1
Yashasvi222 <yashasviagrawal7@gmail.com>
2024-04-13 22:50:54 +0530

    Home page hero section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+import Store from './Pages/Store';
+import Journey from './Pages/Journey';
+import Contact from './Pages/Contact';
+import Cart from './Pages/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/store' element={<Store/>}/>
+        <Route path='/journey' element={<Journey/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
