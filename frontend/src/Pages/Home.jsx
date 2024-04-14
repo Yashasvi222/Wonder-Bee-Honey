9e416519ee29fc7ea6992337c1e7f38f7b2da411
Yashasvi222 <yashasviagrawal7@gmail.com>
2024-04-14 15:16:58 +0530

    Home Page quote section added

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import Quote from '../Components/Quote/Quote';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Quote/>
     </div>
   )
 }
