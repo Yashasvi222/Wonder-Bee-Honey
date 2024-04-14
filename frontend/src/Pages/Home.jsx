c90a5b49fce0ffa78c463d616702ad1c84575696
Yashasvi222 <yashasviagrawal7@gmail.com>
2024-04-14 18:13:30 +0530

    Home Page usp section added

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
 import Quote from '../Components/Quote/Quote';
+import Usp from '../Components/Usp/Usp';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Quote/>
+      <Usp/>
     </div>
   )
 }
